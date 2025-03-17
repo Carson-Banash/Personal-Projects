1227891c7f5697733a6ae245c7373eaf9981edb9
Carson-Banash <cmbanash@gmail.com>
2025-03-17 00:28:18 -0600

    added comments for the dividend section

--- Stock Ticker companion/rolls.py
+++ Stock Ticker companion/rolls.py
@@ -133,40 +133,45 @@ while True:
             if result[poss] < 1000:
                 print("NO DIVIDEND!")
             else:
+                #dictionary required for getting values from the database 
                 player_sec_key = {'grain':4,'ind':5,'bonds':6,'oil':7,'silver':8,'gold':9}
-                owned_sec = ['grain','ind','bonds','oil','silver','gold']
 
+                #gets the current security market values
                 cursor.execute("SELECT DISTINCT name FROM player_info;")
                 result = cursor.fetchall() 
-                players = [item for t in result for item in t]
-                print(players)
-                print(ch_sec)
+
+                #creates a list of the list of tuples returned by the database
+                players = [item for name in result for item in name]
+
+                #initializes the list that will be used to create the popup telling the banker what the payout should be to each player
                 amm_payable = []
+                #loops though all the active players
                 for player in players:
+                    #gets the most recent entry from the database for the player
                     cursor.execute("SELECT * FROM player_info WHERE name = '"+player+"' AND RECENT=(SELECT max(RECENT) FROM player_info WHERE name='"+player+"');")
                     player_info = list(cursor.fetchone())
+                    #gets the previous cash amount for the player
                     cash = player_info[3]
-
+                    #calculates the amount to payout as the multiplication of the amount of the security owned and the rate chosen by the dice roll
                     div_payout = int(player_info[player_sec_key[ch_sec]])*int(ch_amm)*10
-
+                    #calculates the new cash value as the addition of the old cash amount and the dividend payout
                     new_cash = div_payout+cash
 
                     # print(f"{player}'s old cash ${cash} in addition to the payout of ${div_payout} is ${new_cash}")
                     # print(f"{player}'s amount owned {player_info[player_sec_key[ch_sec]]} changed amount {ch_amm} payout is ${div_payout}\n")
 
+                    #creates the string for the player that will be displayed in the popup window for the amount of the payout to receive
                     msg = str(f"{player}'s payout is ${div_payout}\n")
+                    #extends the ongoing list of payout messages
                     amm_payable.extend(msg)
                     
+                    #creates the new player info that will be added to the database. the only values that are changed are the recent modifier, the new amount of cash held by the player and the new net worth
                     new_player_info = [None,player_info[1]+1,player_info[2],new_cash]+player_info[4:10]+[player_info[10]+div_payout]
+                    #adds the new player entry to the database 
                     cursor.execute("INSERT INTO player_info VALUES (?,?,?,?,?,?,?,?,?,?,?)", (new_player_info))
                     connection.commit()
 
+                #creates the message for the popup window as the combination of all the messages from the different players
                 popup_msg = ''.join([str(i) for i in amm_payable])
+                #makes the popup with the message created above
                 sg.popup(popup_msg, title='Dividend Payout')
-                
-                #TODO: get all players amount of the security and do math to figure out how much money to add to their total
-                
-            
-
-
-    
