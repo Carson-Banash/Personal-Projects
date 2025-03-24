4de521cd29530b46e9a00411dc9b9829b3df573b
Carson-Banash <cmbanash@gmail.com>
2025-03-24 14:01:44 -0600

    added winner calculation and a window to display the podium places. also uncommented all the code that makes the database

--- Stock Ticker companion/start.py
+++ Stock Ticker companion/start.py
@@ -46,6 +46,9 @@ database = 'test.db'
 # connection.commit()
 
 #----------------------------------#
+#This function makes the starting window
+#the starting window gets how many players there are and how many rolls there are before buy/sell
+#it returns the amount of players and the rolls
 def start():
    sg.set_options(font=('Arial', 16))
    layout1 = [
@@ -56,18 +59,29 @@ def start():
       [sg.Slider(range=(2,20),default_value=5,orientation='horizontal',key='rolls_between')],
       [sg.Button('Submit',key='player_entry'), sg.Button('Exit')]
    ]
-
    start_window = sg.Window('Roll Window', layout1, finalize=True) 
+
    while True:
+        #reads the values from the window
         event, values = start_window.read()
-        if event == 'player_entry':
+
+        #if the exit button is selected then close the window and exit the program
+        if event in (sg.WIN_CLOSED, 'Exit'):
+            print('Ending Program!')
+            start_window.close()
+            exit()
+            break
+        
+        #if the submit button is selected then get the chosen values and save them. then close the window
+        elif event == 'player_entry':
             num_of_p = values['num_players']
             amm_rolls = int(values['rolls_between'])
             start_window.close()
             break
-
+    #return the user inputted values
    return num_of_p,amm_rolls
 
+#this function 
 def last_round():
     lr_layout = [
         [sg.Text('You have indicated that this will be the last few rolls before the end of the game!\nPlease indicate below how many rolls each player should have before the game is over.')],
@@ -87,6 +101,59 @@ def last_round():
 
     return lr_amm_rolls
 
+def game_end(database):
+    connection = sqlite3.connect(database)
+    cursor = connection.cursor()
+    #gets the current players
+    cursor.execute("SELECT DISTINCT name FROM player_info;")
+    result = cursor.fetchall() 
+
+    #creates a list of the list of tuples returned by the database
+    players = [item for name in result for item in name]
+    win_order = []
+    for player in players:
+        cursor.execute("SELECT * FROM player_info WHERE name = '"+player+"' AND RECENT=(SELECT max(RECENT) FROM player_info WHERE name='"+player+"');")
+        player_info = list(cursor.fetchone())
+        net_worth = player_info[10]
+        print(f"{player}'s net worth is: ${net_worth}")
+        win_order.append((player,net_worth))
+    
+    win_order.sort(key=lambda x: x[-1],reverse=True)
+    print(win_order)
+
+    layout = [
+        [sg.Text("The game is now over!!\nthe leaderboard is as follows:")],
+        [sg.Text(key='leaderboard')],
+        [sg.Text("Thanks for playing and using the program!")],
+        [sg.Button("End Program",button_color='red')]
+    ]
+
+    end_window = sg.Window('End Window', layout, finalize=True)
+    place = ['First','Second','Third','Fourth','Fifth','Sixth','Seventh','Last']
+
+    leaderboard_msg = []
+    for i in range(len(win_order)):
+        if i == len(win_order)-1:
+            leaderboard_msg.extend(str(f"in {place[-1]} place is {win_order[i][0]} with a total of ${win_order[i][1]}\n"))
+        else:
+            leaderboard_msg.extend(str(f"in {place[i]} place is {win_order[i][0]} with a total of ${win_order[i][1]}\n"))
+
+    update_msg = ''.join([str(i) for i in leaderboard_msg])
+    # print(update_msg)
+    end_window['leaderboard'].update(update_msg)
+
+    while True:
+        event, values = end_window.read()
+
+        if event in (sg.WIN_CLOSED, 'End Program'):
+            print('Ending Program!')
+            end_window.close()
+            exit()
+            break
+
+        
+
+sg.set_options(font=('Arial', 16))
 
 
 player_number = 1
@@ -106,3 +173,5 @@ lr_rolls = int(last_round())
 print(lr_rolls)
 
 rolls.roll(database,lr_rolls,num_of_p)
+
+game_end(database)
