4a1f9ac3783ea726e6090cc4096bf38a0e2f5310
Carson-Banash <cmbanash@gmail.com>
2025-03-29 14:53:14 -0600

    completely rewrote how the graph is drawn. using lines instead of matplotlib

--- Stock Ticker companion/graph.py
+++ Stock Ticker companion/graph.py
@@ -1,82 +1,74 @@
 import PySimpleGUI as sg
 import sqlite3
 
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
-def create_plot():
-    plt.style.use('dark_background')
-    plt.margins(x=0,y=0)
-
+def create_plot(active):
     database = 'Stock Ticker Game 25,3,2025 13:14.db'
     connection = sqlite3.connect(database)
     cursor = connection.cursor()
-    securities = ['grain','ind','bonds','oil','silver','gold']
+
     color = {'grain':'#EDC643','ind':'#EE7A8D','bonds':'#93A561','oil':'#94B6C5','silver':'#D2C3AB','gold':'#F2A547'}
     
-    for sec in securities:
+    cursor.execute("SELECT ID FROM board_info WHERE ID=(SELECT max(ID) FROM board_info);")
+    tpl = cursor.fetchone()
+    num_of_entries = tpl[0]
+
+    window['graph'].erase()
+    x_step = 1400/num_of_entries
+
+    for sec in active:
         cursor.execute("SELECT "+sec+" FROM board_info;")
         raw_result = cursor.fetchall()
         result = [x[0] for x in raw_result]
         # print(result)
-        
-        plt.plot(result, color=color[sec],lw=0.75)
-        
+        x=0
+        for i in range(num_of_entries-1):
+            window['graph'].draw_line((x,result[i]),(x+x_step,result[i+1]), color=color[sec], width=1) 
+            x=x+x_step       
 
-    plt.hlines(y=[2000, 1000, 0], xmin=0, xmax=len(result), colors=['green','blue','red'], ls='--')
-
-    plt.axis('off')
-    plt.subplots_adjust(left=0.01,right=0.99,bottom=0.01,top=0.99)
-    # plt.show()
-    return plt.gcf()
-
-
-# market_value = []
-# for i in range(len(raw_result)):
-#     market_value.append(raw_result[i][1])
-
-# print(time)
-# print(market_value)
-
-# plt.plot(market_value,color='#94B6C5')
-# plt.title('Value of security over game', fontsize=14)
-
-
-# plt.grid(axis='y')
-# plt.show()
+    for y, color in [(0, 'red'), (1000, 'blue'), (2000, 'green')]:
+        for x in range(-1, 1400, int(x_step)): 
+            window['graph'].draw_line((x, y), (x+1, y), color=color, width=2)
+    
 btn_size = (6,2)
-b1 = sg.Button('Grain', size=btn_size, key='grain',button_color='black on #EDC643')
-b2 = sg.Button('Ind.', size=btn_size, key='ind',button_color='black on #EE7A8D')
-b3 = sg.Button('Bonds', size=btn_size, key='bonds',button_color='black on #93A561')
-b4 = sg.Button('Oil', size=btn_size, key='oil',button_color='black on #94B6C5')
-b5 = sg.Button('Silver', size=btn_size, key='silver',button_color='black on #D2C3AB')
-b6 = sg.Button('Gold', size=btn_size, key='gold',button_color='black on #F2A547')
+b1 = sg.Button('Grain', size=btn_size, key='grain',button_color='white on red')
+b2 = sg.Button('Ind.', size=btn_size, key='ind',button_color='white on red')
+b3 = sg.Button('Bonds', size=btn_size, key='bonds',button_color='white on red')
+b4 = sg.Button('Oil', size=btn_size, key='oil',button_color='white on red')
+b5 = sg.Button('Silver', size=btn_size, key='silver',button_color='white on red')
+b6 = sg.Button('Gold', size=btn_size, key='gold',button_color='white on red')
 
-buttons = [[b1],[b2],[b3],[b4],[b5],[b6]]
 layout = [
-    [sg.Column(buttons,element_justification='center'),sg.Canvas(size=(1275,920),key='graph',pad=(0,0))]      
+    [sg.Graph(
+        canvas_size=(1400, 800),
+        graph_bottom_left=(-1, -50),
+        graph_top_right=(1400, 2100),
+        background_color='black',
+        key='graph')],
+        [sg.Text(expand_x=True,background_color='grey',size=(1,6)),b1,b2,b3,b4,b5,b6,sg.Text(expand_x=True,background_color='grey',size=(1,6))],
           ]
 
-def draw_figure(canvas, figure):
-    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
-    figure_canvas_agg.draw()
-    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
-    return figure_canvas_agg
 
-window = sg.Window("plot",layout, finalize=True, margins=(0,0))
+window = sg.Window("plot",layout, finalize=True,element_justification='c',margins=(0,0),background_color='black')
 
-draw_figure(window['graph'].TKCanvas, create_plot())
 
 securities = ['grain','ind','bonds','oil','silver','gold']
 colors = {'grain':'black on #EDC643','ind':'black on #EE7A8D','bonds':'black on #93A561','oil':'black on #94B6C5','silver':'black on #D2C3AB','gold':'black on #F2A547'}
+active = []
 
-clicked = True
+
 while True:
     event, values = window.read()
 
     if event == sg.WIN_CLOSED:
         break
     if event in securities:
-        
-        window[event].update(button_color='white on red' if clicked else colors[event])
-        clicked = not clicked
+        old_color = window[event].ButtonColor
+        if old_color[1] == 'red':
+            window[event].update(button_color=colors[event])
+            active.append(event)
+
+        else:
+            window[event].update(button_color='white on red')
+            active.remove(event)
+    
+    create_plot(active)
